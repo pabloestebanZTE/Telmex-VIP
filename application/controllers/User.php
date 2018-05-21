2633018325e95b96cdf0b9d7224911036e6553ac
pabloestebanZTE <pablo.esteban@zte.com.cn>
2018-05-21 14:22:44 -0500

    algoritmo de marcaciones

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -97,69 +97,95 @@ class User extends CI_Controller {
 
         $prefijos = $this->input->post('pref');
 
-        for ($i=0; $i < count($prefijos); $i++) { 
-            $pref[substr($prefijos[$i], 0, 3 )][$i] = substr($prefijos[$i], 5,2);  
+        for ($i=0; $i < count($prefijos); $i++) {
+          if($prefijos[$i][3] != "0"){
+            $pref[substr($prefijos[$i], 0, 3 )][$i] = substr($prefijos[$i], 3,4);
+          } else {
+            if($prefijos[$i][4] != "0"){
+              $pref[substr($prefijos[$i], 0, 3 )][$i] = substr($prefijos[$i], 4,3);
+            } else{
+              if($prefijos[$i][5] != "0"){
+                $pref[substr($prefijos[$i], 0, 3 )][$i] = substr($prefijos[$i], 5,2);
+              } else {
+                $pref[substr($prefijos[$i], 0, 3 )][$i] = substr($prefijos[$i], 6,1);
+              }
+            }
+          }
+        //    $pref[substr($prefijos[$i], 0, 3 )][$i] = substr($prefijos[$i], 3,4);
         }
 
 
-
-        print_r($pref);
   /************************************************************************************************************/
-        $res = "";
+        $j = 0;
         foreach ($pref as $i => $valores) {
-            $res .= "$i";      
-        }  
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  /************************************************************************************************************/
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            $res[$j] = "$i";
+            $j++;
+        }
+        $respuesta = "";
+        for($i = 0; $i < count($res); $i++){
+          $string[$i] = $res[$i];
+          $flag = 0;
+          sort($pref[$res[$i]]);
+          $pref[$res[$i]][0] = $this->fixZeros($pref[$res[$i]][0]);
+          //RECORRE EL ARREGLO
+          for($j = 0; $j < count($pref[$res[$i]]); $j++){
+            //VERIFICA SI EL ARREGLO TIENE MAS DE UN ELEMENTO
+            if(count($pref[$res[$i]]) != 1){
+            //  echo "$flag<br>";
+              //VERIFICA QUE NO LLEGUE AL ULTIMO ELEMENTO DEL ARREGLO
+              if($j != (count($pref[$res[$i]])-1)){
+                //COMPARA SI EL NUMERO VA PARA UNA SUCESION
+                if($pref[$res[$i]][$j+1] == ($pref[$res[$i]][$j]) + 1){
+                  //echo "com =  1<br>";
+                  if($flag == 0){
+                    //NO VIENE DE SUCESION Y VA PARA SUCESION, PINTA NUMERO -
+                      $string[$i] = $string[$i].$pref[$res[$i]][$j]."-";
+                  } else {
+                    //NO HACE NADA PORQUE VIENE DE SUCESION Y SIGUE EN SUCESION
+                  }
+                  $flag = 1;
+                } else {
+                  //echo "com =  0<br>";
+                  //VERIFICA SI VIENE DE UNA SUCESION
+                  if($flag == 0){
+                      //NO VIENE DE SUCESION Y NO VA PARA SUCESION, NUMERO,
+                      $string[$i] = $string[$i].$pref[$res[$i]][$j].",";
+                  } else {
+                    //NO VIENE DE SUCESION Y NO VA PARA SUCESION, NUMERO,
+                      $string[$i] = $string[$i].$pref[$res[$i]][$j].",";
+                  }
+                  $flag = 0;
+                }
+              } else {
+                //COMO ES EL ULTIMO VALOR, SE PINTA EL NUMERO SOLO
+                $string[$i] = $string[$i].$pref[$res[$i]][$j];
+              }
+            } else {
+              $string[$i] = $string[$i].$pref[$res[$i]][0];
+            }
+          }
+          if($i != (count($res)-1)){
+            $respuesta = $respuesta.$string[$i].",";
+          } else {
+              $respuesta = $respuesta.$string[$i];
+          }
+        }
+        $this->json($respuesta);
     }
 
-
+    public function fixZeros($x){
+      if(strlen($x) == 1){
+      $x = "000".$x;
+      }
+      if(strlen($x) == 2){
+      $x = "00".$x;
+      }
+      if(strlen($x) == 3){
+      $x = "0".$x;
+      }
+      return $x;
+    }
 
 }
 
 ?>
-
